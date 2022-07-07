b372fae7d54905e92075f3cfc1b3c44fbddda3eb
Xartyago <santiago.salcedo@mercadolibre.com.co>
2022-07-07 11:16:42 -0500

    Change Struct

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/Xartyago/XartyaGo-/transaction"
+	"github.com/gin-gonic/gin"
+)
+
+var Transactions []transaction.Transaction
+
+//This endpoint fill our var Transaction and show them
+func getAndUpdateAllTransactions(c *gin.Context) {
+	// Query statements
+	var realData map[string]string
+	possibleQueries := map[string]string{
+		"id":       c.Query("Id"),
+		"name":     c.Query("Name"),
+		"code":     c.Query("Code"),
+		"currency": c.Query("Currency"),
+		"emisor":   c.Query("Emisor"),
+		"receiver": c.Query("Receiver"),
+		"amount":   c.Query("Amount"),
+		"date":     c.Query("Date"),
+	}
+	for key, query := range possibleQueries {
+		if query != "" {
+			realData[key] = query
+		}
+	}
+
+	//Get without queries
+	if len(Transactions) == 0 {
+		var transactions []transaction.Transaction
+		transactionsJson := transaction.Json()
+		json.Unmarshal([]byte(transactionsJson), &transactions)
+		*&Transactions = transactions
+		c.JSON(200, gin.H{"Transactions": transactions})
+	}
+}
+
+// Here we go to find a transaction by id
+func getSpecificTransaction(c *gin.Context) {
+	idToFound, _ := strconv.Atoi(c.Param("id"))
+	trcionFounded := Transactions[idToFound-1]
+	if trcionFounded.Id > 0 {
+		c.JSON(200, trcionFounded)
+	} else {
+		c.JSON(404, gin.H{"msg": "The tranac not founded"})
+	}
+}
 
 func main() {
-	fmt.Println("Funca")
+	router := gin.Default()
+	transactions := router.Group("/transactions")
+	{
+		transactions.GET("/", getAndUpdateAllTransactions)
+		transactions.GET("/:id", getSpecificTransaction)
+	}
+	router.Run(":3001")
 }
--- transaction/transaction.go
+++ transaction/transaction.go
@@ -2,13 +2,16 @@ package transaction
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 type Transaction struct {
-	Id                                                           int
-	TransactionCode, Currency, Emisor, Receiver, TransactionDate string
-	Amount                                                       float64
+	Id              int     `json:"id"`
+	TransactionCode string  `json:"transactioncode"`
+	Currency        string  `json:"currency"`
+	Emisor          string  `json:"emisor"`
+	Receiver        string  `json:"receiver"`
+	TransactionDate string  `json:"transactiondate"`
+	Amount          float64 `json:"amount"`
 }
 
 func CreateTransactions() []Transaction {
@@ -17,14 +20,20 @@ func CreateTransactions() []Transaction {
 		{Id: 2, TransactionCode: "1B", Currency: "ARS", Emisor: "Rafael", Receiver: "Xarts S.A.S.", TransactionDate: "31/06/2022", Amount: 400.10},
 		{Id: 3, TransactionCode: "1C", Currency: "CLP", Emisor: "Miguel", Receiver: "Tyago", TransactionDate: "20/03/2022", Amount: 92.13},
 		{Id: 4, TransactionCode: "1D", Currency: "MXN", Emisor: "Angel", Receiver: "Pepito Store's", TransactionDate: "15/05/2022", Amount: 40.1},
+		{Id: 5, TransactionCode: "1E", Currency: "USD", Emisor: "Sebastian", Receiver: "Pinturitas", TransactionDate: "20/10/2021", Amount: 100.50},
+		{Id: 6, TransactionCode: "1F", Currency: "ARS", Emisor: "Nicolas", Receiver: "Luxury", TransactionDate: "02/01/2022", Amount: 1000},
+		{Id: 7, TransactionCode: "1G", Currency: "EUR", Emisor: "Fabian", Receiver: "Auri", TransactionDate: "20/03/2022", Amount: 12.15},
+		{Id: 8, TransactionCode: "1H", Currency: "MXN", Emisor: "Manuel", Receiver: "Delivery cat", TransactionDate: "15/05/2022", Amount: 50.3},
+		{Id: 9, TransactionCode: "1I", Currency: "COP", Emisor: "Nelson", Receiver: "Santi's food", TransactionDate: "06/04/2022", Amount: 92.12},
+		{Id: 10, TransactionCode: "1J", Currency: "EUD", Emisor: "Sofia", Receiver: "Comican", TransactionDate: "29/06/2022", Amount: 43.2},
+		{Id: 11, TransactionCode: "1K", Currency: "CLP", Emisor: "Lizeth", Receiver: "Dollarcity", TransactionDate: "18/03/2022", Amount: 800.43},
+		{Id: 12, TransactionCode: "1L", Currency: "MXN", Emisor: "Mia", Receiver: "Pirotecnic", TransactionDate: "15/05/2022", Amount: 2.12},
 	}
 	return transactions
 }
 
-func Json() {
+func Json() []byte {
 	transactions := CreateTransactions()
 	transactionsJson, _ := json.Marshal(transactions)
-	var reconverter []Transaction
-	json.Unmarshal([]byte(transactionsJson), &reconverter)
-	fmt.Println(reconverter)
+	return transactionsJson
 }
